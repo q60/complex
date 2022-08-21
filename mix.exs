8adb09e76562e732cd114ce2e66e56ee85d481cb
llathasa-veleth <llathasa@outlook.com>
2022-08-21 22:12:07 +0300

    1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Complex.MixProject do
   def project do
     [
       app: :complex,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       description: description(),
